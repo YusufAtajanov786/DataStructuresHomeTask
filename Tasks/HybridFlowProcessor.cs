2e7cecb93952c99f6af3a800b99218f71a51e665
Yusufboy Atajanov <Yusufboy_Atajanov@epam.com>
2022-03-27 18:50:53 +0500

    Task 2 Complited

--- Tasks/HybridFlowProcessor.cs
+++ Tasks/HybridFlowProcessor.cs
@@ -3,26 +3,93 @@ using Tasks.DoNotChange;
 
 namespace Tasks
 {
+
+    public class DoubleNode<T>
+    {
+        public T data { get; }
+        public DoubleNode<T> next { get; set; }
+        public DoubleNode<T> previous { get; set; }
+
+        public DoubleNode(T value)
+        {
+            data = value;
+            next = null;
+            previous = null;
+        }
+    }
     public class HybridFlowProcessor<T> : IHybridFlowProcessor<T>
     {
+        public DoubleNode<T> head;
+
         public T Dequeue()
         {
-            throw new NotImplementedException();
+            if (head == null)
+                throw new InvalidOperationException();
+            DoubleNode<T> last = head;
+            DoubleNode<T> previous = head;
+            T data;
+            while (last.next != null)
+            {
+                previous = last;
+                last = last.next;
+            }
+            previous.next = null;
+            data = last.data;
+            return data;
         }
 
         public void Enqueue(T item)
         {
-            throw new NotImplementedException();
+            DoubleNode<T> node = new DoubleNode<T>(item);
+            
+            if(head == null)
+            {
+                head = node;
+                return;
+            }
+
+            head.previous = node;
+            node.next = head;                
+            head = node;
         }
 
         public T Pop()
         {
-            throw new NotImplementedException();
+            if(head == null)
+            {
+                throw new InvalidOperationException();
+            }
+
+            DoubleNode<T> last = head;
+            DoubleNode<T> previous = head;
+            T data ;
+            while (last.next != null)
+            {
+                previous = last;
+                last = last.next;
+            }
+            data = last.data;
+            previous.next = null;
+            return data;
         }
 
         public void Push(T item)
         {
-            throw new NotImplementedException();
+            DoubleNode<T> node = new DoubleNode<T>(item);
+            if(head == null)
+            {
+                head = node;
+                return;
+            }
+            node.next = null;
+            DoubleNode<T> last = head;
+            while (last.next != null)
+            {
+                last = last.next;
+            }
+            node.previous = last;
+            last.next = node;
+            
         }
     }
 }
